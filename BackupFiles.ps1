a4b8d22bceb169079cedc74148103168c510868d
venu <venurshan.manivannan@gmail.com>
2024-12-07 18:03:43 +0100

    First commit

---
+++ BackupFiles.ps1
@@ -0,0 +1,17 @@
+﻿$sourceDir = "C:\Users\Venu\Desktop\Test"
+$destinationDir = "D:\BackUp"
+
+$date = Get-Date -Format "yyyy-MM-dd"
+
+$destinationMonthDir = Join-Path -Path $destinationDir -ChildPath $date
+
+if (-not (Test-Path $destinationMonthDir -PathType Container)) {
+    New-Item -Path $destinationMonthDir -ItemType Directory | Out-Null
+}
+
+$filesToCopy = Get-ChildItem -Path $sourceDir | Where-Object { -not $_.PSIsContainer }
+
+foreach ($file in $filesToCopy) {
+    $destinationPath = Join-Path -Path $destinationMonthDir -ChildPath $file.Name
+    Copy-Item -Path $file.FullName -Destination $destinationPath
+}
